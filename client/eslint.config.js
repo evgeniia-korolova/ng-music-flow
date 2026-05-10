efd7cbcf4241e50717c39c94004cc420a4079dcb
evgeniia-korolova <jkod@ukr.net>
2026-05-10 15:56:51 +0300

    test: stabilize Vitest browser mode with AnalogJS, setup linter, prettier, husky

---
+++ client/eslint.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const unusedImports = require('eslint-plugin-unused-imports');
+const unicorn = require('eslint-plugin-unicorn').default;
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    plugins: {
+      'unused-imports': unusedImports,
+      unicorn,
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'no-unused-vars': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/prefer-top-level-await': 'off',
+      'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+      "unicorn/no-null": "off",
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
