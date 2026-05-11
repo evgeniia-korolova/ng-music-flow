3637cd3a5dec3321f293e9d3ca8512e2c6aabddb
evgeniia-korolova <jkod@ukr.net>
2026-05-11 18:13:46 +0300

    fix: fix ci-pipeline.yml for tests

--- client/src/test-setup.ts
+++ client/src/test-setup.ts
@@ -1,6 +1,7 @@
 import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-snapshots';
 import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
+import '@analogjs/vitest-angular/setup-vitest';
 
 setupTestBed({
   browserMode: true, // Enables visual test preview
