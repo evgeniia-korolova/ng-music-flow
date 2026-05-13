63d58ffa07551a75678471ebe12c4975592d66a7
Vitiok2009 <viktorpts1vol@gmail.com>
2026-05-13 19:45:18 +0300

    feat(artist): create artists page, configure routing and move widget

--- client/src/app/widgets/artists-list/artists-list.ts
+++ client/src/app/widgets/artists-list/artists-list.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ArtistCard } from '../../entities/artist/ui/artist-card/artist-card';
+import { ArtistStore } from '../../entities/artist/model/artist.store';
 
 @Component({
   selector: 'app-artists-list',
-  imports: [],
+  imports: [ArtistCard],
   templateUrl: './artists-list.html',
   styleUrl: './artists-list.scss',
 })
-export class ArtistsList {}
+export class ArtistsList {
+  store = inject(ArtistStore);
+}
