13cc9bebb5240199babfa29e8fa6531647db54a7
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-14 16:25:38 +0400

    fix: fix linter issue in api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port);
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  const logger = new Logger('Shutdown');
+  logger.error('Application failed to start');
+  logger.error(err);
+  process.exit(1);
+});
