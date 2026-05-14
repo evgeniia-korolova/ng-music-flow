628b70d00fa9dcd96925e01a40cbb25f733ed676
Vitiok2009 <viktorpts1vol@gmail.com>
2026-05-14 16:57:52 +0300

    feat: finalize artist card UI with tailwind

--- client/src/app/entities/artist/ui/artist-card/artist-card.ts
+++ client/src/app/entities/artist/ui/artist-card/artist-card.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Artist } from '../../model/artist.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-artist-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './artist-card.html',
   styleUrl: './artist-card.scss',
 })
