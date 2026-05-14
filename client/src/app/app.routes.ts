39c1a43c88f7fd8aacb0224c0ca89d1345f2d167
evgeniia-korolova <jkod@ukr.net>
2026-05-15 00:58:02 +0300

    refactor: move layouts to app layer

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from './shared/ui/layouts/main-layout/main-layout';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
   {
