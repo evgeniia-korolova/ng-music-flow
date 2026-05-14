09b694c338453961b04c9eff745555cf4f22e262
Vitiok2009 <viktorpts1vol@gmail.com>
2026-05-14 21:19:58 +0300

    test: fix vite optimizeDeps and update spec files

--- client/src/app/entities/artist/ui/artist-card/artist-card.spec.ts
+++ client/src/app/entities/artist/ui/artist-card/artist-card.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArtistCard } from './artist-card';
+import { provideRouter } from '@angular/router';
 
 describe('ArtistCard', () => {
   let component: ArtistCard;
@@ -9,10 +10,12 @@ describe('ArtistCard', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ArtistCard],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ArtistCard);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('artist', { id: '1', name: 'Test Artist' });
     await fixture.whenStable();
   });
 
