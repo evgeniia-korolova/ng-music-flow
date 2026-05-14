19b47cc2c9bf325f7918e2f2dfacf11745288006
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-14 19:21:38 +0400

    chore: add linter for tailwind

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,22 +5,32 @@ const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const unusedImports = require('eslint-plugin-unused-imports');
 const unicorn = require('eslint-plugin-unicorn').default;
+import betterTailwindcss from 'eslint-plugin-better-tailwindcss';
 
-module.exports = defineConfig([
+export default defineConfig([
   {
     files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
     ],
     plugins: {
       'unused-imports': unusedImports,
-      unicorn,
+      'unicorn': unicorn,
+      'better-tailwindcss': betterTailwindcss,
     },
     processor: angular.processInlineTemplates,
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: './src/tailwind-theme.css',
+      },
+    },
     rules: {
+      'better-tailwindcss/no-unknown-class': 'warn',
+      'better-tailwindcss/sort-class-names': 'warn',
+
       '@angular-eslint/directive-selector': [
         'error',
         {
@@ -46,7 +56,16 @@ module.exports = defineConfig([
   },
   {
     files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
+    plugins: {
+      'better-tailwindcss': betterTailwindcss,
+    },
+    languageOptions: {
+      parser: await import('@angular-eslint/template-parser'),
+    },
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {
+      'better-tailwindcss/no-unknown-class': 'warn',
+      'better-tailwindcss/sort-class-names': 'warn',
+    },
   },
 ]);
