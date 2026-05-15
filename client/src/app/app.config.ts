21c107db38f9308e6261be70e3c8010e7a4aa338
evgeniia-korolova <jkod@ukr.net>
2026-05-15 15:11:06 +0300

    chore: remove environment.development from git tracking, add Zone.js

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +11,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(),
   ],
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  jamendoClientId: '',
+  apiUrl: 'https://api.jamendo.com/v3.0',
+};
