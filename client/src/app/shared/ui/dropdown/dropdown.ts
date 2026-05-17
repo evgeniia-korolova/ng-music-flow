a55598a55e8c821df82e8353023a9d83babbe050
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-17 22:31:23 +0400

    fix: fix dropdown

--- client/src/app/shared/ui/dropdown/dropdown.ts
+++ client/src/app/shared/ui/dropdown/dropdown.ts
@@ -1,12 +1,13 @@
 import { NgTemplateOutlet } from '@angular/common';
 import {
   Component,
-  ContentChild,
-  ContentChildren,
+  contentChild,
+  contentChildren,
+  effect,
   ElementRef,
   HostListener,
   inject,
-  QueryList,
+  input,
   signal,
   TemplateRef,
 } from '@angular/core';
@@ -20,21 +21,38 @@ import {
 export class Dropdown {
   isOpen = signal(false);
   private readonly el = inject(ElementRef);
+  alignment = input<'left' | 'right'>('left');
 
-  @ContentChild('trigger', { read: ElementRef }) triggerButton!: ElementRef<HTMLElement>;
+  constructor() {
+    effect(() => {
+      const el = this.triggerButton()?.nativeElement;
 
-  @ContentChildren('item') dropdownItems!: QueryList<TemplateRef<HTMLElement>>;
+      if (!el) return;
+
+      if (this.isOpen()) {
+        el.dataset['active'] = 'true';
+      } else {
+        delete el.dataset['active'];
+      }
+    });
+  }
+
+  triggerButton = contentChild<ElementRef<HTMLElement>>('trigger');
+  dropdownItems = contentChildren<TemplateRef<unknown>>('item');
 
   @HostListener('document:click', ['$event'])
   handleClick(event: Event) {
     const clickedElement = event.target as HTMLElement;
+    const triggerEl = this.triggerButton()?.nativeElement;
 
-    if (this.triggerButton?.nativeElement.contains(clickedElement)) {
+    const clickedInsideDropdown = this.el.nativeElement.contains(clickedElement);
+
+    if (triggerEl === clickedElement || triggerEl?.contains(clickedElement)) {
       this.isOpen.update((prev) => !prev);
       return;
     }
 
-    if (!this.el.nativeElement.contains(clickedElement)) {
+    if (!clickedInsideDropdown) {
       this.isOpen.update(() => false);
     }
   }
