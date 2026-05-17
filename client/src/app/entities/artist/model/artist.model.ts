c8123a8356fe8cb27da1e89fc66c6c2fb405252b
Vitiok2009 <viktorpts1vol@gmail.com>
2026-05-17 17:52:58 +0300

    feat: add artist signal store and fetch data from API

--- client/src/app/entities/artist/model/artist.model.ts
+++ client/src/app/entities/artist/model/artist.model.ts
@@ -1,3 +1,13 @@
+export interface ArtistDto {
+  id: string;
+  name: string;
+  website: string;
+  joindate: string;
+  image: string;
+  shorturl: string;
+  shareurl: string;
+}
+
 export interface Artist {
   id: string;
   name: string;
