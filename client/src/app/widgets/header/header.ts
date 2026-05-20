c8ac5e0f6ae8e35d9d9a9f23416bd9aa6c606a76
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-20 16:34:47 +0400

    feat: add navlinks to header

--- client/src/app/widgets/header/header.ts
+++ client/src/app/widgets/header/header.ts
@@ -3,13 +3,29 @@ import { LucideDynamicIcon } from '@lucide/angular';
 import { Dropdown } from '../../shared/ui/dropdown/dropdown';
 import { ThemeToggler } from '../../features/theme-toggler/theme-toggler';
 import { ThemeStore } from '../../features/theme-toggler/theme.store';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { DropdownCloseDirective } from '../../shared/ui/dropdown/dropdown-close.directive';
 
 @Component({
   selector: 'app-header',
-  imports: [LucideDynamicIcon, Dropdown, ThemeToggler],
+  imports: [
+    LucideDynamicIcon,
+    Dropdown,
+    ThemeToggler,
+    RouterLink,
+    DropdownCloseDirective,
+    RouterLinkActive,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   themeStore = inject(ThemeStore);
+  private readonly router = inject(Router);
+
+  get navItems() {
+    const routes = this.router.config.flatMap((layout) => layout.children || []);
+
+    return routes.filter((route) => route?.data?.['displayOnNavbar'] === true);
+  }
 }
