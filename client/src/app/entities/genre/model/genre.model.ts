5e2a065ab8937516d9a14be6bb4d0e7fc38eb377
Vitiok2009 <viktorpts1vol@gmail.com>
2026-05-22 23:34:27 +0300

    test: fix test environment and add DOM check for genre card

--- client/src/app/entities/genre/model/genre.model.ts
+++ client/src/app/entities/genre/model/genre.model.ts
@@ -1,5 +1,67 @@
 export interface Genre {
-  id: string;
-  title: string;
-  image: string;
+  readonly id: string;
+  readonly title: string;
+  readonly image: string;
 }
+export const GENRES_DATA: readonly Genre[] = [
+  {
+    id: 'pop',
+    title: 'Pop',
+    image: '/genres/pop.webp',
+  },
+  {
+    id: 'rock',
+    title: 'Rock',
+    image: '/genres/rock.webp',
+  },
+  {
+    id: 'electronic',
+    title: 'Electronic',
+    image: '/genres/electronic.webp',
+  },
+  {
+    id: 'hiphop',
+    title: 'Hip-Hop',
+    image: '/genres/hiphop.webp',
+  },
+  {
+    id: 'jazz',
+    title: 'Jazz',
+    image: '/genres/jazz.webp',
+  },
+  {
+    id: 'classical',
+    title: 'Classical',
+    image: '/genres/classical.webp',
+  },
+  {
+    id: 'lounge',
+    title: 'Lounge',
+    image: '/genres/lounge.webp',
+  },
+  {
+    id: 'relaxation',
+    title: 'Relaxation',
+    image: '/genres/relaxation.webp',
+  },
+  {
+    id: 'songwriter',
+    title: 'Songwriter',
+    image: '/genres/songwriter.webp',
+  },
+  {
+    id: 'world',
+    title: 'World',
+    image: '/genres/world.webp',
+  },
+  {
+    id: 'metal',
+    title: 'Metal',
+    image: '/genres/metal.webp',
+  },
+  {
+    id: 'soundtrack',
+    title: 'Soundtrack',
+    image: '/genres/soundtrack.webp',
+  },
+] as const;
--- client/src/app/entities/genre/ui/genre-card/genre-card.spec.ts
+++ client/src/app/entities/genre/ui/genre-card/genre-card.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GenreCard } from './genre-card';
+import { provideRouter } from '@angular/router';
 
 describe('GenreCard', () => {
   let component: GenreCard;
@@ -9,14 +10,21 @@ describe('GenreCard', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [GenreCard],
+      providers: [provideRouter([])],
     }).compileComponents();
 
+    const mockGenre = { id: 'test', title: 'title', img: '/genres/pop.webp' };
     fixture = TestBed.createComponent(GenreCard);
+    fixture.componentRef.setInput('genre', mockGenre);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should display genre title in h3', () => {
+    const h3Comp = fixture.nativeElement.querySelector('h3');
+    expect(h3Comp.textContent).toContain('title');
   });
 });
