08df7e629dc5cb91e7b708cd86fc4536aa2639b6
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-25 21:18:03 +0400

    docs: add developer note for mero93 on 05-25

--- client/src/test-setup.ts
+++ client/src/test-setup.ts
@@ -1,7 +1,7 @@
 import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-snapshots';
 import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
-import { provideRouter } from '@angular/router'; // <-- Import this
+import { provideRouter } from '@angular/router';
 
 setupTestBed({
   browserMode: false,
