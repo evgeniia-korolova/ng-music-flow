460eaf944e272d442887c4dfa1a9d981493b84e1
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-25 13:38:34 +0400

    test: fix test dependency issues

--- client/src/app/features/navigation-bar/navigation-bar.spec.ts
+++ client/src/app/features/navigation-bar/navigation-bar.spec.ts
@@ -1,10 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationBar } from './navigation-bar';
+import { Route } from '@angular/router';
 
 describe('NavigationBar', () => {
   let component: NavigationBar;
   let fixture: ComponentFixture<NavigationBar>;
+
+  const dummyRoutes: Route[] = [
+    {
+      path: '',
+      children: [
+        {
+          path: 'discover',
+          title: 'Discover',
+          data: { displayOnNavbar: true },
+          children: [
+            { path: '', redirectTo: 'popular', pathMatch: 'full' },
+            {
+              path: 'popular',
+              title: 'Popular Tracks',
+              data: { order: 'popularity_total', pageTitle: 'Popular Tracks' },
+            },
+            {
+              path: 'new',
+              title: 'New Releases',
+              data: { order: 'releasedate_desc', pageTitle: 'New Releases' },
+            },
+            {
+              path: 'genres',
+              title: 'Genres',
+            },
+          ],
+        },
+        {
+          path: 'artists',
+          title: 'Artists',
+          data: { displayOnNavbar: true },
+        },
+        {
+          path: '',
+          redirectTo: 'discover',
+          pathMatch: 'full',
+        },
+      ],
+    },
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +54,9 @@ describe('NavigationBar', () => {
 
     fixture = TestBed.createComponent(NavigationBar);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('pathCollection', dummyRoutes);
+
     fixture.detectChanges();
   });
 
