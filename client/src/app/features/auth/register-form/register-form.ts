5927f9c2d5c5eb95f942bd4156014e4a244150b8
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-30 15:18:47 +0400

    feat: add register form

--- client/src/app/features/auth/register-form/register-form.ts
+++ client/src/app/features/auth/register-form/register-form.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form } from '@angular/forms/signals';
+import { Input } from '../../../entities/track/ui/input/input';
+
+interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [Input],
   templateUrl: './register-form.html',
   styleUrl: './register-form.scss',
 })
-export default class RegisterForm {}
+export default class RegisterForm {
+  registerModel = signal<RegisterData>({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  registerForm = form(this.registerModel);
+}
