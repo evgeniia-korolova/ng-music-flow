8d81c0680dd0527c613e1def8a9da8e1ce516701
mero93 <merabkopaleishvili1993@gmail.com>
2026-06-01 18:44:55 +0400

    feat: implement login form

--- client/src/app/features/auth/login-form/login-form.ts
+++ client/src/app/features/auth/login-form/login-form.ts
@@ -1,15 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { form } from '@angular/forms/signals';
+import { Component, computed, inject, signal } from '@angular/core';
+import { email, form, required } from '@angular/forms/signals';
 import { Input } from '../../../entities/track/ui/input/input';
+import { Button } from '../../../shared/ui/button/button';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { Icon } from '../../../shared/ui/icon/icon.component';
+import { AuthStore } from '../../../entities/user/user.state';
+import { IconName } from '../../../shared/ui/icon/icon-registry';
 
-interface LoginData {
+export interface LoginData {
   email: string;
   password: string;
 }
 
 @Component({
   selector: 'app-login-form',
-  imports: [Input],
+  imports: [Input, Button, CommonModule, RouterLink, Icon],
   templateUrl: './login-form.html',
   styleUrl: './login-form.scss',
 })
@@ -19,5 +25,50 @@ export default class LoginForm {
     password: '',
   });
 
-  loginForm = form(this.loginModel);
+  store = inject(AuthStore);
+  router = inject(Router);
+
+  loginForm = form(this.loginModel, (schema) => {
+    required(schema.email, { message: 'Email is required' });
+    email(schema.email, { message: 'Must be a correct email' });
+
+    required(schema.password, { message: 'Password is required' });
+  });
+
+  generalError = signal('Oh no! something went wrong on server-side');
+  generalMessage = computed<{ icon: IconName; message: string }>(() => {
+    if (this.generalError()) {
+      return { icon: 'warning', message: this.generalError() };
+    }
+
+    return { icon: 'info', message: 'Fill in your email and password to access your account!' };
+  });
+
+  submitButton = computed<{ icon: IconName; message: string }>(() => {
+    if (this.store.loading()) {
+      return { icon: 'refresh', message: 'Authenticating...' };
+    }
+
+    return { icon: 'logIn', message: 'Submit' };
+  });
+
+  async onSubmit() {
+    if (!this.loginForm().valid()) {
+      this.generalError.set('Something went wrong');
+      return;
+    }
+
+    const formData = this.loginForm().value();
+
+    await this.store.login(formData);
+
+    if (this.store.error()) {
+      this.generalError.set(this.store.error()!.message);
+      return;
+    }
+
+    this.generalError.set('');
+
+    this.router.navigateByUrl('/discover');
+  }
 }
