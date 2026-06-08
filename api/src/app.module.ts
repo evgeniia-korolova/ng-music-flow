34fa0388d2213eaac3ba4639f5120efc42bbe329
mero93 <merabkopaleishvili1993@gmail.com>
2026-06-08 17:17:19 +0400

    feat: add typeorm setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DataModule } from './data/data.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    DataModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['../.env', '.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
