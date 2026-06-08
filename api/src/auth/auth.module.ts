3ffb0087cb93fa154f4eeae0d5e2df2b1b870915
mero93 <merabkopaleishvili1993@gmail.com>
2026-06-08 22:33:35 +0400

    feat: add auth.service, add jwt.strategy, setup jwt encoding options

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const expiresInMinutes = Number.parseInt(
+          configService.get<string>('JWT_EXPIRES_IN', '15'),
+          10,
+        );
+
+        const expiresInSeconds = expiresInMinutes * 60;
+
+        return {
+          secret: configService.getOrThrow<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: expiresInSeconds,
+          },
+        };
+      },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, PassportModule, JwtModule],
 })
 export class AuthModule {}
