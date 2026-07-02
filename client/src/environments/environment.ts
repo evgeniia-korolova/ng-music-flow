bb54cb6f9ff1644454ebc0dfbeb330d39b6e29c7
mero93 <merabkopaleishvili1993@gmail.com>
2026-07-03 02:51:34 +0400

    setup: update production environments

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
+  production: true,
   jamendoClientId: '',
   jamendoClientSecret: '',
   apiUrl: '/api',
-  appApiUrl: '',
+  appApiUrl: 'https://ng-music-flow.onrender.com',
 };
