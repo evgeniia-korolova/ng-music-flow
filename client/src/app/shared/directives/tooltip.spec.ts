46664c24981e034677d7f5b714c0622f2781db52
evgeniia-korolova <jkod@ukr.net>
2026-07-06 00:20:57 +0300

    feat: add thre tests for defferable view in track-list.spec.ts

--- client/src/app/shared/directives/tooltip.spec.ts
+++ client/src/app/shared/directives/tooltip.spec.ts
@@ -25,14 +25,12 @@ describe('TooltipDirective', () => {
     fixture = TestBed.createComponent(TestHostComponent);
     hostComponent = fixture.componentInstance;
 
-    // Находим кнопку, на которой висит директива
     const buttonDebug = fixture.debugElement.query(By.css('button'));
     buttonEl = buttonDebug.nativeElement;
 
     fixture.detectChanges(); // Первая инициализация
   });
 
-  // Чистим DOM после каждого теста, так как директива рендерит поповер в body
   afterEach(() => {
     const remainingTooltips = document.querySelectorAll('.global-tooltip');
     remainingTooltips.forEach((el) => el.remove());
@@ -43,11 +41,9 @@ describe('TooltipDirective', () => {
   });
 
   it('should create and show tooltip element on mouseenter', () => {
-    // Имитируем наведение мыши на кнопку (mouseenter)
     buttonEl.dispatchEvent(new MouseEvent('mouseenter'));
     fixture.detectChanges();
 
-    // Ищем созданный поповер в документе (document.body)
     const tooltipEl = document.querySelector('.global-tooltip');
 
     expect(tooltipEl).toBeTruthy();
@@ -56,17 +52,14 @@ describe('TooltipDirective', () => {
   });
 
   it('should remove tooltip element on mouseleave', () => {
-    // Сначала наводим, чтобы он создался
     buttonEl.dispatchEvent(new MouseEvent('mouseenter'));
     fixture.detectChanges();
 
     expect(document.querySelector('.global-tooltip')).toBeTruthy();
 
-    // Теперь убираем мышь (mouseleave)
     buttonEl.dispatchEvent(new MouseEvent('mouseleave'));
     fixture.detectChanges();
 
-    // Проверяем, что элемент полностью удалился из DOM
     expect(document.querySelector('.global-tooltip')).toBeNull();
   });
 
@@ -74,30 +67,24 @@ describe('TooltipDirective', () => {
     buttonEl.dispatchEvent(new MouseEvent('mouseenter'));
     fixture.detectChanges();
 
-    // Кликаем по кнопке (click)
     buttonEl.dispatchEvent(new MouseEvent('click'));
     fixture.detectChanges();
 
-    // Проверяем, что тултип скрылся при клике (наше требование для шторки)
     expect(document.querySelector('.global-tooltip')).toBeNull();
   });
 
   it('should dynamically update text when input signal changes', () => {
-    // Наводим
     buttonEl.dispatchEvent(new MouseEvent('mouseenter'));
     fixture.detectChanges();
 
     expect(document.querySelector('.global-tooltip')?.textContent).toBe('Initial Tooltip');
 
-    // Убираем мышь, чтобы сбросить стейт
     buttonEl.dispatchEvent(new MouseEvent('mouseleave'));
     fixture.detectChanges();
 
-    // ВАЖНО: Меняем значение сигнала в хост-компоненте
     hostComponent.tooltipText.set('Updated Tooltip text');
     fixture.detectChanges();
 
-    // Наводим снова и проверяем реактивность инпута
     buttonEl.dispatchEvent(new MouseEvent('mouseenter'));
     fixture.detectChanges();
 
---
+++ development-notes/evgeniia-korolova/evgeniia-korolova-sprint-4-2026-06-30.md
@@ -0,0 +1,21 @@
+# Start Date: 2026-23-06 second week
+
+# End Date: 2026-29-06
+
+**What I did:**
+
+- completed library/history
+- add origin and playedAt to library
+- unit test for @defer study
+  [article](https://angular.love/learn-how-to-unit-test-the-deferrable-views)
+  tracks-list.spec.ts
+  implement test-cases for two levels: block provider.isLoading(), provider.error(), provider.tracks() and inner block with @defer
+
+**Code Review**
+first 3 sprints code review was mostly in PRs, 4th sprint communicated mostly in Discord because coded on fly. I pulled team-mates braches to my local, tryed myself, something was lost during conflicts and restored, sometimes worked on the same branch because Library is a result of all three members efforts and we had to wait for each other
+
+**Problems & SolutionsIssue**:
+
+- **Time spent:** 4-8 hour per day - total about 22 hours
+
+- **Plans for the second week**
