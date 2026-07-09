d81caf3c51999b924a6bc1ad0eeef9b187c93441
vitiok2021 <viktorpts1vol@gmail.com>
2026-07-09 21:44:38 +0300

    fix: added tests

--- client/src/app/pages/artist-profile/artist-profile.spec.ts
+++ client/src/app/pages/artist-profile/artist-profile.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ArtistProfile } from './artist-profile';
 import { provideRouter } from '@angular/router';
 import { signal } from '@angular/core';
@@ -8,7 +7,7 @@ import { ArtistStore } from '../../entities/artist/model/artist.store';
 describe('ArtistProfile', () => {
   const mockArtistStore = {
     isLoading: signal(false),
-    error: signal(null),
+    error: signal<string | null>(null),
     currentArtist: signal({ id: '2', name: 'Viktor' }),
     albums: signal([]),
     tracks: signal([]),
@@ -35,5 +34,18 @@ describe('ArtistProfile', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should display loading message', () => {
+    mockArtistStore.isLoading.set(true);
+    fixture.detectChanges();
+    const loadingText = fixture.nativeElement.querySelector('.artist-profile__loading');
+    expect(loadingText.textContent).toContain('loading');
+  });
+  it('should display error message', () => {
+    mockArtistStore.isLoading.set(false);
+    mockArtistStore.error.set('Something went wrong');
+    fixture.detectChanges();
+    const errorText = fixture.nativeElement.querySelector('.artist-profile__loading');
+    expect(errorText.textContent).toContain('Oops!');
   });
 });
