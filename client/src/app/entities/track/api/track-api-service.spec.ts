b38db88a3aadac7b10fdb41c171f47b941bcc4ca
vitiok2021 <viktorpts1vol@gmail.com>
2026-07-10 23:32:19 +0300

    test: add unit tests for artist profile, album card and track api service

--- client/src/app/entities/track/api/track-api-service.spec.ts
+++ client/src/app/entities/track/api/track-api-service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { TrackApiService } from './track-api-service';
+import { environment } from '../../../../environments/environment';
 
 describe('TrackApiService', () => {
   let service: TrackApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(TrackApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should get custom track titles', () => {
+    const mockTitles = [{ id: '1', title: 'My Awesome Track' }];
+
+    service.getCustomTrackTitles().subscribe((titles) => {
+      expect(titles).toEqual(mockTitles);
+    });
+
+    const req = httpTestingController.expectOne(`${environment.appApiUrl}/tracks/titles`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTitles);
   });
 });
